97d8f6813aa541618fc94aaf5caf25b9f67df9d5
luiscarvil <1888@holbertonschool.com>
2020-06-11 22:04:17 +0000

    alphabet lower and upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,11 @@ int main(void)
 char x;
 char y;
 
-for (x = 'a'; x <= 'z'; x++)
+for (x = 'a'; x <= 'z' ; x++)
 {
 putchar(x);
 }
-for (y = 'A'; y<= 'Z'; y++)
+for (y = 'A'; y<= 'Z' ; y++)
 {
 putchar(y);
 }
