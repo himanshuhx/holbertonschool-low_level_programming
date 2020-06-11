9e6441e33073925e6d206c5d7bf0ab216adf7b30
luiscarvil <1888@holbertonschool.com>
2020-06-11 21:13:02 +0000

    lower and upper char

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Retunr:Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+char y;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+for (y = 'a'; y<= 'z'; y++)
+{
+putchar(y);
+}
+return (0);
+}
