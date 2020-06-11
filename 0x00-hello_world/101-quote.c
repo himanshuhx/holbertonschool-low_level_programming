8a714c4de3fc65f7e6f052369cacea20bf2028fd
luiscarvil <1888@holbertonschool.com>
2020-06-11 01:26:26 +0000

    print // write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *Description : it is
+ * Return: Always 1  (error)
  */
-int main (void)
+int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
