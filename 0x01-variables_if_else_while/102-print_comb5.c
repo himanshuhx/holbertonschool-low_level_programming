2fb89836c83a25fdfcbe04e8bcde6bae5c833cb9
luiscarvil <1888@holbertonschool.com>
2020-06-12 20:56:02 +0000

    4 numbers c for

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - conten about the main suilcar
+ *
+ *
+ *Return : 0 (Success)
+ */
+int main(void)
+{
+  int x;
+  int y;
+  int z; 
+  int w;
+  for (x = '0'; x <= '9'; x++)
+    {
+      for (y = '0'; y < '9'; y++)
+	{
+	  for (z = x; z <= '9'; z++)
+	    { 
+	      for (w = y + 1; w <= '9'; w++)
+		{
+	  putchar(x);
+	  putchar(y);
+	  putchar(' ');
+	  putchar(z);
+	  putchar(w);
+	  putchar(',');
+	  putchar(' ');
+		}
+	    }
+	}
+      
+    }
+  putchar('\n');
+  return(0);
+}
