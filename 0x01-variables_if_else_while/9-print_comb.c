f4dfa561182e21e2487e9702ea5fe15f5f6bec72
luiscarvil <1888@holbertonschool.com>
2020-06-12 01:45:19 +0000

    putchar , " "

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for ( x = '0'; x <= '9'; x++)
+{
+putchar(x);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
