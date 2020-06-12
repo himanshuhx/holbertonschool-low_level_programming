3eb355d3966b3db929bda35ca2c2db08e7724fe6
luiscarvil <1888@holbertonschool.com>
2020-06-12 00:14:14 +0000

    putchar alphab --

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for ( x = 'z' ; x >= 'a' ; x--)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+} 
