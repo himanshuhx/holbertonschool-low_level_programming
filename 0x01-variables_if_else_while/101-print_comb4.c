352e0d9165dc26eeda3de0db758074158921979b
luiscarvil <1888@holbertonschool.com>
2020-06-12 04:46:28 +0000

    print num 012 - 789

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int x;
+  int y;
+  int z;
+  for (x = '0'; x <= '9'; x++)
+    {
+      y = x + 1;
+      for (; y <= '9'; y++)
+	{
+	  for(z = y +1; z <= '9'; z++)
+	    {
+	  putchar(x);
+	  putchar(y);
+	  putchar(z);
+	  if (x == '7' && y == '8' && z == '9')
+	    {
+	      break;
+	    }
+	  putchar(',');
+	  putchar(' ');
+	    }	
+	}
+    }
+  putchar('\n');
+  return (0);
+}
