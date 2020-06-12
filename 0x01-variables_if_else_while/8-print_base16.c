66e006cb495e086e80bba5d254ffb9c75dc64137
luiscarvil <1888@holbertonschool.com>
2020-06-12 00:37:07 +0000

    print 0-9 abcdef

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+}
+for (x = 'a'; x <= 'f'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
