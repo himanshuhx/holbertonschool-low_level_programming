3997750ec51a366b54e1baa9d349bd92ff34c8d7
luiscarvil <1888@holbertonschool.com>
2020-06-12 04:22:28 +0000

    no repeat num

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,24 @@
  */
 int main(void)
 {
-int x;
-int y;
-for (x = '0'; x <= '9'; x++)
-{
-y = x+1;
-for (; y <= '9'; y++)
-{
-putchar(x);
-putchar(y);
-if (x == '8' && y == '9')
-{
-break;
-}
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+  int x;
+  int y;
+
+  for (x = '0'; x <= '9'; x++)
+    {
+      y = x + 1;
+      for (; y <= '9'; y++)
+	{
+	  putchar(x);
+	  putchar(y);
+	  if (x == '8' && y == '9')
+	    {
+	      break;
+	    }
+	  putchar(',');
+	  putchar(' ');
+	}
+    }
+  putchar('\n');
+  return (0);
 }
