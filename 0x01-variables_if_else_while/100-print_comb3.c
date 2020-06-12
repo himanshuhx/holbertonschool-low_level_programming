a065f44309584a57f1a2e8129151e0fd23c49f41
luiscarvil <1888@holbertonschool.com>
2020-06-12 04:03:48 +0000

    no repeat num

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+int y;
+for (x = '0'; x <= '9'; x++)
+{
+for (y = x + 1; y <= '9'; y++)
+{
+putchar(x);
+putchar(y);
+if ( x == '8' && y == '9')
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
