5650bf370aec474ea8de853e7d54d666e421dd64
luiscarvil <1888@holbertonschool.com>
2020-06-16 19:09:51 +0000

    lower case with _putchar heder -c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
-
 /**
  *
  * _islower - checks lowercase
- *
+ *@c: for integers
+ * Return: Always 0 (Success)
  */
-
-
 int _islower(int x)
 {
 if (x >='a' && x <= 'z')
+{
 return (1);
+}
 else
+{
 return (0);
+}
 } 
