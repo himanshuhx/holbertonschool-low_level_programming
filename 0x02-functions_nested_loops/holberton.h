c6c95367a2f54807b47b97645a770cde5d8ce1dc
luiscarvil <1888@holbertonschool.com>
2020-06-16 20:02:42 +0000

    actualiz .h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,7 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-int _putchar(char x);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int x);
+int _islower(int c);
+int _isalpha(int c);
 #endif
