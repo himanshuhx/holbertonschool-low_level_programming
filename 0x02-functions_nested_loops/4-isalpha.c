dcc340c8b46a2af21e70cf6e15339fd3890c3726
luiscarvil <1888@holbertonschool.com>
2020-06-16 19:57:19 +0000

    return 1 if lower or upper return 0 else

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *_isalpha - lowercase or uppercase 1 else 0
+ *@c: integers
+ *Return: Always 0 (Success
+ */
+int _isalpha(int c)
+{
+
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
