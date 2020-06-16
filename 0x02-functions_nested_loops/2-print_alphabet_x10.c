5f1ed1f484d2ba4ce9870e91ba80456ce427258a
luiscarvil <1888@holbertonschool.com>
2020-06-16 18:43:53 +0000

    print alphabet with function void

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 /**
- * main -check the code for Hoblerton School students.
+ * void print_alphabet - print
+ * @void: void argument
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-  char x;
-  int z;
-  z = 0;
-  while (z <= 9)
-    {
-      for (x = 'a'; x <= 'z'; x++)
-	{
-	  _putchar(x);
-	}
-      _putchar('\n');
-      z++;
-    }
-
+char x;
+int z;
+z = 0;
+while (z <= 9)
+{
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+z++;
+}
 } 
