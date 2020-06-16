c38e50369376b785efc13ae53b6ab3590b1583e3
luiscarvil <1888@holbertonschool.com>
2020-06-16 20:41:57 +0000

    sign (+, 1) (0, 0 (-, /)

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * print_sign - print - 0 or + sign a numb
- * @c: integer
+ *print_sign - print - 0 or + sign a numb
+ *@n: integer
  *
- * Return: Always 0 (Successs)
+ *Return: Always 0 (Successs)
  */
 int print_sign(int n)
 {
@@ -17,7 +17,7 @@ else if (n == 0)
 _putchar('0');
 return (0);
 }
-else 
+else
 {
 _putchar('-');
 return (-1);
