c936d526a1d1c46ee6c0df3df22f93a546cd6883
luiscarvil <1888@holbertonschool.com>
2020-06-16 18:52:33 +0000

    alphabet x10 head c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 /**
- * void print_alphabet - print
+ * print_alphabet_x10  - print alphabet*10
  * @void: void argument
  *
  * Return: Always 0 (Success)
+ *
+ *
  */
 void print_alphabet_x10(void)
 {
