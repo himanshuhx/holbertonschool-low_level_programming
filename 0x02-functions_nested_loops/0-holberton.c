5d8441cc39d714da9a4696ad167967a5dd134d47
luiscarvil <1888@holbertonschool.com>
2020-06-16 17:45:52 +0000

    header print Holberton _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-  char x[] = "Holberton\n";
-  int y;
-  for ( y = 0; y <=9; y++)
-    {
-      _putchar(x[y]);
-    }
-  return (0);
+char x[] = "Holberton\n";
+int y;
+for (y = 0; y <= 9; y++)
+{
+_putchar(x[y]);
+}
+return (0);
 }
