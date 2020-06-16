fbfda767159fe73a3b0dd40ef8eeeb8f9b8d0b94
luiscarvil <1888@holbertonschool.com>
2020-06-16 18:26:14 +0000

    _puthcar alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ *
+ */
+void print_alphabet(void)
+    {
+      char x;
+  for (x = 'a'; x <= 'z'; x++)
+    {
+      _putchar(x);
+    }
+  _putchar('\n');
+}
