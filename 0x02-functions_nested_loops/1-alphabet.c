517c8e3bfd29b4d1eb3298ecd8d6e106695c93ac
luiscarvil <1888@holbertonschool.com>
2020-06-16 18:28:32 +0000

    _puthcar alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  *
  */
 void print_alphabet(void)
-    {
-      char x;
-  for (x = 'a'; x <= 'z'; x++)
-    {
-      _putchar(x);
-    }
-  _putchar('\n');
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
 }
