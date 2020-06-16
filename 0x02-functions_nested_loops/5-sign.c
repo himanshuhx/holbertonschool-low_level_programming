b385a017275709eb99d1a348479d70ca344a1384
luiscarvil <1888@holbertonschool.com>
2020-06-16 20:39:30 +0000

    sign (+, 1) (0, 0 (-, /)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_sign - print - 0 or + sign a numb
+ * @c: integer
+ *
+ * Return: Always 0 (Successs)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else 
+{
+_putchar('-');
+return (-1);
+}
+}
