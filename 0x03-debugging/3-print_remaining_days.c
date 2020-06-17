743f277940fb68893a79f8b19a6af8c6b8f2f75e
luiscarvil <1888@holbertonschool.com>
2020-06-17 18:13:41 +0000

    print fay and year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0 || year % 400 != 0) && (year % 100 == 0))
+if ((year % 400 == 0 || year % 100 == 0) && (year % 4 == 0))
 {
 if (month >= 3 && day >= 60)
 {
@@ -20,7 +20,7 @@ day++;
 }
 
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+printf("Remaining days: %d\n", 365 - day);
 }
 else
 {
