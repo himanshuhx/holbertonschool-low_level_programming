e868f1b89af81e7aa9f3178a98d85159619512ba
luiscarvil <1888@holbertonschool.com>
2020-06-17 04:38:45 +0000

    first debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *main - test function positive or negative
+ *
+ *Return: 0 Always (Success)
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
