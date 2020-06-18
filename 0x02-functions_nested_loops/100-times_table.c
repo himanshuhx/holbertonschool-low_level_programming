b8da453f058d598fce821c06080df7e9b2bc533d
luiscarvil <1888@holbertonschool.com>
2020-06-18 00:20:51 +0000

    table 3 5 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,50 +2,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * print_times_table - print tables 
- * @n: integers
- * Return: Always 0 (Success)
+ *print_times_table - print tables
+ *@n: integers
+ *Return: Always 0 (Success)
  */
 void print_times_table(int n)
 {
-  int a, b, c;
-  if ( n >=0 && n <= 13)
-    {
-  for (a = 0; a <= n; a++)
-    {
-    for (b = 0; b <= n; b++)
-      {
-	c = a * b;
-	if (c <= 9)
-	  {
-	    if (b != 0)
-	      {
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(' ');
-	    _putchar(' ');
-	      }
-	    _putchar(c % 10 + '0');
-	  }
-	else if (c <= 99)
-	  {
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(' ');
-	    _putchar(c / 10 + '0');
-	    _putchar(c % 10 + '0');
-	  }
-	else if (c >= 100)
-	  {
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(c / 100 + '0');
-	    _putchar((c / 10) % 10 + '0');
-	    _putchar(c % 10 + '0');
-	  }
-      }
-    _putchar('\n');
-    }
-
-    } 
+int a, b, c;
+if (n >= 0 && n <= 13)
+{
+for (a = 0; a <= n; a++)
+{
+for (b = 0; b <= n; b++)
+{
+c = a * b;
+if (c <= 9)
+{
+if (b != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
+_putchar(c % 10 + '0');
+}
+else if (c <= 99)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(c / 10 + '0');
+_putchar(c % 10 + '0');
+}
+else if (c >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(c / 100 + '0');
+_putchar((c / 10) % 10 + '0');
+_putchar(c % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
 }
