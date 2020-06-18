57a0ad91744d02358eac7469fecbae2aabc5a9a4
luiscarvil <1888@holbertonschool.com>
2020-06-18 03:03:28 +0000

    fibonacci 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print 50 fibonacci numbers.
+ * Return: Always 0.
+ */
+int main(void)
+{
+float a, b, c;
+int d;
+a = 0;
+b = 1;
+for (d = 0; d < 99; d++)
+{
+c = a + b;
+a = b;
+b = c;
+printf("%.0f", c);
+if (d == 99)
+{
+printf("\n");
+}
+else
+{
+printf(", ");
+}
+}
+return (0);
+}
