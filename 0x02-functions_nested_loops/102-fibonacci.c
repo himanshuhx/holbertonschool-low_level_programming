706d7b305a8a4efbdb7de4807579ea366995acf8
luiscarvil <1888@holbertonschool.com>
2020-06-18 02:09:18 +0000

    serie fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print 50 fibonacci numbers.
+ * Return: Always 0.
+ */
+int main(void)
+{
+  long int a, b, c;
+  int d;
+  a = 0;
+  b = 1;
+  for (d = 0; d < 50; d++)
+    {
+      c = a + b;
+      a = b;
+      b = c;
+      printf("%ld", c);
+      if ( d == 49)
+	{
+	  printf("\n");
+	}
+      else
+	{
+	  printf(", ");
+	}
+
+    }
+  return (0);
+}
