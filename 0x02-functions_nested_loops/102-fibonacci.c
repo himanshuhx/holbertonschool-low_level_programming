c6396aa287cd8e1bc4d2d8ecc1584437903df969
luiscarvil <1888@holbertonschool.com>
2020-06-18 02:12:11 +0000

    serie fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,25 +5,24 @@
  */
 int main(void)
 {
-  long int a, b, c;
-  int d;
-  a = 0;
-  b = 1;
-  for (d = 0; d < 50; d++)
-    {
-      c = a + b;
-      a = b;
-      b = c;
-      printf("%ld", c);
-      if ( d == 49)
-	{
-	  printf("\n");
-	}
-      else
-	{
-	  printf(", ");
-	}
-
-    }
-  return (0);
+long int a, b, c;
+int d;
+a = 0;
+b = 1;
+for (d = 0; d < 50; d++)
+{
+c = a + b;
+a = b;
+b = c;
+printf("%ld", c);
+if (d == 49)
+{
+printf("\n");
+}
+else
+{
+printf(", ");
+}
+}
+return (0);
 }
