a6372a34f5ff0d9c4ef106ab83075371466377a4
luiscarvil <1888@holbertonschool.com>
2020-06-18 20:18:28 +0000

    multiplicar 2 numeros

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *mul - mult 2 numbers
+ *@a: first integer
+ *@b: second integer
+ *Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,5 +2,5 @@
 #define HOLBERTON_H
 int _putchar(char c);
 int _isupper(int c);
-
+int mul(int a, int b);
 #endif
