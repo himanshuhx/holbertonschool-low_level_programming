b7e43f8a70bd3f281199d67712b880a2ab8178ab
luiscarvil <1888@holbertonschool.com>
2020-06-19 05:44:40 +0000

    make a triangle with blank spaces and #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+B#include "holberton.h"
+/**
+ *print_triangle - print triangle with spaces and #.
+ *@size: integer
+ *Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size > 0)
+	{
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+		c = size - (a + 1);
+			if (b < c)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
