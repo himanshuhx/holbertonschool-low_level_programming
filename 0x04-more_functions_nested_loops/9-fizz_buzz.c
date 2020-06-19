a2ea8af4d72723ffdc8ae5d01b3ec85067dcfea7
luiscarvil <1888@holbertonschool.com>
2020-06-19 03:48:34 +0000

    fizz buzz print

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *main - print all number and Fizz Buzz words
+ *
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 1;
+	for (; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
