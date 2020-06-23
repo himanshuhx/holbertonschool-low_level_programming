71c7ffc3cf085cb8599ba10ad947320eb02d3b07
luiscarvil <1888@holbertonschool.com>
2020-06-23 20:31:41 +0000

    first pointer in c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - take a pointer to an int as parameter and update the value
+ *@n: is a pointer
+ *Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
