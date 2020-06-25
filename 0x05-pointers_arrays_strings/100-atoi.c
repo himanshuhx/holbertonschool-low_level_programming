54f3abadf4d07ce4cb7646d32771dae07139475c
luiscarvil <1888@holbertonschool.com>
2020-06-25 02:34:23 +0000

    convert string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *_atoi - function that convert string to an integer
+ *@s: pointer.
+ *Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	int a = 0, b = 1, c = 0;
+
+	while (s[a] != '\0')
+	{
+		if (s[a] == '-')
+		{
+			b = b * -1;
+		}
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			c = (c * 10) + (s[a] - '0');
+		}
+		a++;
+		if (s[a] == ';')
+		{
+			break;
+		}
+	}
+	return (b = b * c);
+}
